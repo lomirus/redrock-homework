c42e38b5c420e61433cc3804dcc5d2ba8bae0150
Rhys <lomirus@qq.com>
2021-04-02 21:09:05 +0800

    some improments

--- web-front-end/3rd-spring/example/src/index.js
+++ web-front-end/3rd-spring/example/src/index.js
@@ -2,8 +2,11 @@ import axios from 'schoolwork-1'
 axios.get('https://anonym.ink/api/home/sections')
     .then(data => {
         JSON.parse(data).data.map(section => {
-            section.List.map(v => console.log(v))
-            section.Rank.map(v => console.log(v))
+            section.List.map(v => {
+                console.log("Title: ", v.Title)
+                console.log("Description: ", v.Description)
+                console.log('')
+            })
         })
     })
     .catch(err => {
--- web-front-end/3rd-spring/package/axios.js
+++ web-front-end/3rd-spring/package/axios.js
@@ -1,6 +1,5 @@
 const axios = ({ method, url }) => new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
-    xhr.setHeader("Access-Control-Allow-Origin", "*");  
     xhr.onreadystatechange = () => {
         if (xhr.readyState == 4) {
             if (xhr.status == 200) {
